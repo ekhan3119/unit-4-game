775a5cd857ee50e73a2004d1fc8c58cea51a08d9
ekhan3119 <ekc3119@gmail.com>
2018-12-15 10:28:14 -0500

    added code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,70 @@
 //declare variables
 var startingNumber;
-var crystalValues;
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
 //generating random number for crystals
-var randomNumber = Math.floor((Math.random()* 12)+ 1);
 
 
 //when page load generate a random number between 19-120
 $(document).ready (function(){
-    console.log ('I am working')
-function startGame(){
-    startingNumber = Math.floor((Math.random()* 120)+ 19);
-    $("#random").text("Match this number: " + startingNumber);
-    
-    //update wins, losses, totalScore 
-    $("#wins").text("Wins: "+ wins);
-    $("#losses").text("Losses: "+ loses);
-    $("#your-score").text("Your total score: "+ totalScore);
+        console.log ('I am working')
+    function startGame(){
+        startingNumber = Math.floor((Math.random()* 101)+ 19);
+        $("#random").text("Match this number: " + startingNumber);
+        
+        //update wins, losses, totalScore 
+        $("#wins").text("Wins: "+ wins);
+        $("#losses").text("Losses: "+ losses);
+        $("#your-score").text("Your total score: "+ totalScore);
+        crystalRandomNumber("#blue-diamond");
+        crystalRandomNumber("#red-heart");
+        crystalRandomNumber("#orange-octagon");
+        crystalRandomNumber("#green-cube");
 
-    //Generate a random number for each crystals
-    newRandomNumber("#diamond");
-    newRandomNumber("#red-heart");
-    newRandomNumber("#orange-octagon");
-    newRandomNumber("#green-cube");
-}
-
-function crystalRandomNumber(id){
-    for (i = 0; i< 4; i++){
-    randomNumber = Math.floor((Math.random()* 12)+ 1);
+        //$("#blue-diamond").attr("data-value", Math.floor(Math.random() * 11+1) );
     }
-$("#blue-diamond").att("value", randomNumber);
-}
-function whenGmaeStart(){
-    $(".diamond").on("click",crystalRandomNumber);
-}
-startGame();
-whenGmaeStar();
+        
+    //function to get random number for crystals
+    function crystalRandomNumber(id){
+        var randomNumber= Math.floor(Math.random() * 11+1);
+    //generate a random num 
+        $(id).attr("data-value", randomNumber);
+    //assign that num with that id
+    }
+    //click event for crystals
+    $(".crystal").on("click", function (){
+        console.log("clicked");
+        var crystalValue = $(this).data("value");
+        console.log(crystalValue);
+        totalScore += crystalValue;
+        if (totalScore === startingNumber){
+            wins++;
+            reset();
+            //$("#wins").text()
+        } else { totalScore > startingNumber 
+            losses++;
+            reset();
+        }
+        
+    });
 
-//$("#random").load(Math.floor((Math.random()* 120)+ 19));
-//$("#random").text(startingNumber);
+    function reset(){
+        totalScore = 0;
+        startingNumber;
+        //crystalValue;
+    };
+        //retrive the value for that crystal
+        //add that to their total score
+        //check if their total score matches the target score
+            //update the wins and restart the game
+        //check if their total score is the greater then the target score 
+            //update the losses and restart
+        //else just update their score 
+    
+    startGame();
+
+    //$("#random").load(Math.floor((Math.random()* 120)+ 19));
+    //$("#random").text(startingNumber);
 
 });
